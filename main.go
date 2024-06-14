5942c9bed270151d82c4fc9887d4764ba90e27df
Brendan Wilde <44901073+Adenosperma1@users.noreply.github.com>
2024-06-14 23:15:59 +1000

    looping 3 neurons

--- main.go
+++ main.go
@@ -6,34 +6,29 @@ func main() {
 
 	inputs := []float32{1, 2, 3, 2.5}
 
-	weights0 := []float32{0.2, 0.8, -0.5, 1.0}
-	weights1 := []float32{0.5, -0.91, 0.26, -0.5}
-	weights2 := []float32{-0.26, -0.27, 0.17, 0.87}
+	weights := [][]float32{
+		{0.2, 0.8, -0.5, 1.0},
+		{0.5, -0.91, 0.26, -0.5},
+		{-0.26, -0.27, 0.17, 0.87},
+	}
 
-	var bias0 float32 = 2.0
-	var bias1 float32 = 3.0
-	var bias2 float32 = 0.5
+	biases := []float32{2.0, 3.0, 0.5}
 
-	outputs := make([]float32, 3)
 
-	outputs[0] = (
-		inputs[0]*weights0[0] +
-		inputs[1]*weights0[1] +
-		inputs[2]*weights0[2] +
-		inputs[3]*weights0[3] + bias0)
+	layerOutputs := make([]float32, 3)
 
-	outputs[1] = (
-		inputs[0]*weights1[0] +
-		inputs[1]*weights1[1] +
-		inputs[2]*weights1[2] +
-		inputs[3]*weights1[3] + bias1)
+for i := range weights{
+neuronWeights := weights[i]
+neuroBias := biases[i]
 
-	outputs[2] = (
-		inputs[0]*weights2[0] +
-		inputs[1]*weights2[1] +
-		inputs[2]*weights2[2] +
-		inputs[3]*weights2[3] + bias2)
+var neuronOutput float32 = 0
+for j := range inputs{
+	neuronOutput += inputs[j] * neuronWeights[j]
+}
+neuronOutput += neuroBias
+layerOutputs[i] = neuronOutput
+}
 
-	fmt.Println(outputs)
+	fmt.Println(layerOutputs)
 
 }
